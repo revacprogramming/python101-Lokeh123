25120479abfab2de50a5d56b92f04a747c6c1554
Lokesh J <97089488+Lokeh123@users.noreply.github.com>
2022-05-22 15:09:16 +0000

    problem10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,20 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+counts=dict()
+for line in handle:
+    if not line.startswith("From:"):
+        continue
+    words=line.split()
+    y=(words[1:])
+    for word in y:
+        counts[word]=counts.get(word,0)+1
+ 
+    largestcount = 0
+    largestword =0
+    for word,count in counts.items():
+        if largestcount is 0 or count > largestcount:
+           largestword=word
+           largestcount=count
+print(largestword,largestcount)
--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,19 @@
-# Tuples
+name = raw_input("Enter file:")
+handle = open(name)
+d=dict()
+for line in handle:
+    if not line.startswith("From "): 
+        continue
+    else:    
+        a=line.split()
+        x=a[5]
+        y=x[0:2]
+        d[y]=d.get(y,0)+1
 
-filename = "dataset/mbox-short.txt"
+lst=list()        
+for value,count in d.items():
+    lst.append((value,count))
+
+lst.sort()
+for value,count in lst:
+    print (value,count)
