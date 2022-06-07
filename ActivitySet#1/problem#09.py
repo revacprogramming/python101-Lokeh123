1ce2541c6b558e2c3507f4d5d9ff417e7ea41fdb
Lokesh J <97089488+Lokeh123@users.noreply.github.com>
2022-06-07 15:30:22 +0000

    Problem 9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,7 +1,7 @@
 # fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()                       
-for line in fh:                    
+for lin e in fh:                    
     word= line.split()    
     for elem e nt in word:             
         if element in lst:         
