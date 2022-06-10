e87b56827371a738ec5b1dd0f730c8bd3de74b81
Lokesh J <97089488+Lokeh123@users.noreply.github.com>
2022-06-10 12:57:07 +0000

    Problem 4

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,8 +1,8 @@
-hrs = input("Enter Hours:")
+gfhrs = input("Enter Hours:")
 h = float(hrs)
 rate=input ("enter rate:")
 f= float( rate)
-if h>40:
+if h>4 0:
     b=h*f
     a=(h-40)*(f*0.5)
     c=a+b
